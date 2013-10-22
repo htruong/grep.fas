a503fb4f9f6c464f838205efa32cd6bb650b57d2
Huan Truong <huant@tnhh.net>
2013-10-22 17:26:28 -0500

    FEATURE: Reading genes from a list

--- grep.fas.go
+++ grep.fas.go
@@ -23,6 +23,49 @@ var printDescr = flag.Bool("d", true, "Prints description lines")
 var printSeq = flag.Bool("s", true, "Prints sequences lines")
 var printFirst = flag.Bool("1", false, "Match the first sequence only")
 var getNumberedSeq = flag.Bool("n", false, "Match the numbered sequences instead of matching strings, separated by commas (,)")
+var geneList = flag.String("l", "", "File containing list of genes to be extracted (it will match any)")
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// writeLines writes the lines to the given file.
+func writeLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
+}
+
+func matchAny(needles []string, haystack string) bool {
+	for _, s := range needles {
+		matched, _ := regexp.MatchString(s, haystack)
+		if matched {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 
@@ -31,11 +74,19 @@ func main() {
 	s := ""
 	numberedSeq := make(map[int]bool)
 	seqCounter := 0
+	var genesToMatch []string
 
 	// We need to check if we have enough params given
-	if flag.NArg() == 0 {
+	if (flag.NArg() == 0) && (geneList == nil) {
 		log.Fatal("Fatal error: Search pattern is needed.")
 		os.Exit(-2)
+	} else if geneList != nil {
+		var err error
+		genesToMatch, err = readLines(*geneList)
+		if err != nil {
+			log.Fatal("Fatal error: Cannot read the file containing list of genes to search for.")
+			os.Exit(-2)
+		}
 	} else {
 		s = flag.Args()[0]
 		if *getNumberedSeq {
@@ -73,12 +124,16 @@ func main() {
 			seqMatched := false
 
 			if !*getNumberedSeq {
-				matched, err := regexp.MatchString(s, scanner.Text())
-				if err != nil {
-					log.Fatal(err)
-					os.Exit(-1)
+				if geneList == nil {
+					var err error
+					seqMatched, err = regexp.MatchString(s, scanner.Text())
+					if err != nil {
+						log.Fatal(err)
+						os.Exit(-1)
+					}
+				} else {
+					seqMatched = matchAny(genesToMatch, scanner.Text())
 				}
-				seqMatched = matched
 			} else {
 				seqMatched = numberedSeq[seqCounter]
 			}
